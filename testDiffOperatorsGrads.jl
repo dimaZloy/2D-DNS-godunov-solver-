287b60a4dbb799bb96638666eefd6b82f52c912c
dimaZloy <46398066+dimaZloy@users.noreply.github.com>
2022-10-04 21:12:50 +0200

    updated GreenGaussNodesBasedGrad method

--- testDiffOperatorsGrads.jl
+++ testDiffOperatorsGrads.jl
@@ -22,6 +22,141 @@ end
 end
 
 
+@inline function calcPhiInCellCluster(Node::Int32, Cell::Int32, testMesh::mesh2d_Int32, scalarFieldCells::Vector{Float64})::Float64
+
+    phi::Float64 = 0.0;
+    counter::Int64 = 0;
+    for i = 1:testMesh.nNeibCells
+        cell::Int32 = testMesh.cell_clusters[Node,i];
+        if cell > 0 && cell <= testMesh.nCells #&& cell != Cell
+            phi +=  scalarFieldCells[cell];
+            counter += 1;
+        end
+    end
+
+    #return phi/counter;
+    return counter!=0 ? phi/counter : 0.0; 
+
+end 
+
+function calcScalarFieldGreenGaussCellBasedGradient(cellsThreads::Array{Int32,2}, testMesh::mesh2d_Int32, 
+    scalarFieldCells::Vector{Float64}, scalarFieldNodes::Vector{Float64}, gradX::Vector{Float64}, gradY::Vector{Float64})
+  
+  
+    Threads.@threads for p in 1:Threads.nthreads()
+           
+
+                phiFaceX = zeros(Float64,4);
+                phiFaceY = zeros(Float64,4);
+                
+                phiLeft = zeros(Float64,4);
+                phiRight = zeros(Float64,4);
+                
+                side = zeros(Float64,4);
+                nx = zeros(Float64,4);
+                ny = zeros(Float64,4);
+                
+            
+                for C =  cellsThreads[p,1]:cellsThreads[p,2]
+                
+                    
+                    side[1] = testMesh.cell_edges_length[C,1];
+                    side[2] = testMesh.cell_edges_length[C,2];
+                    side[3] = testMesh.cell_edges_length[C,3];
+                    
+                    nx[1] = testMesh.cell_edges_Nx[C,1];
+                    nx[2] = testMesh.cell_edges_Nx[C,2];
+                    nx[3] = testMesh.cell_edges_Nx[C,3];
+                    
+                    ny[1] = testMesh.cell_edges_Ny[C,1];
+                    ny[2] = testMesh.cell_edges_Ny[C,2];
+                    ny[3] = testMesh.cell_edges_Ny[C,3];
+                    
+            
+                    # phiLeft[1] =  scalarField[ testMesh.cells2nodes[C,1] ];
+                    # phiRight[1] = scalarField[ testMesh.cells2nodes[C,2] ];
+            
+                    # phiLeft[2] =  scalarField[ testMesh.cells2nodes[C,3] ];
+                    # phiRight[2] = scalarField[ testMesh.cells2nodes[C,4] ];
+            
+                    # phiLeft[3] =  scalarField[ testMesh.cells2nodes[C,5] ];
+                    # phiRight[3] = scalarField[ testMesh.cells2nodes[C,6] ];
+                    
+                    phiLeft[1]  = calcPhiInCellCluster(testMesh.cells2nodes[C,1], C, testMesh, scalarFieldCells );
+                    phiRight[1] = calcPhiInCellCluster(testMesh.cells2nodes[C,2], C, testMesh, scalarFieldCells );
+                    phiLeft[2]  = calcPhiInCellCluster(testMesh.cells2nodes[C,3], C, testMesh, scalarFieldCells );
+                    phiRight[2] = calcPhiInCellCluster(testMesh.cells2nodes[C,4], C, testMesh, scalarFieldCells );
+                    phiLeft[3]  = calcPhiInCellCluster(testMesh.cells2nodes[C,5], C, testMesh, scalarFieldCells );
+                    phiRight[3] = calcPhiInCellCluster(testMesh.cells2nodes[C,6], C, testMesh, scalarFieldCells );
+            
+
+                    if (testMesh.mesh_connectivity[C,3] == 4) ## if number of node cells == 4 
+                    
+                        side[4] = testMesh.cell_edges_length[C,4];
+                        nx[4] = testMesh.cell_edges_Nx[C,4];
+                        ny[4] = testMesh.cell_edges_Ny[C,4];
+
+                        phiLeft[4]  = calcPhiInCellCluster(testMesh.cells2nodes[C,7], C, testMesh, scalarFieldCells );
+                        phiRight[4] = calcPhiInCellCluster(testMesh.cells2nodes[C,8], C, testMesh, scalarFieldCells );
+    
+                        # phiLeft[4] =  scalarField[ testMesh.cells2nodes[C,7] ];
+                        # phiRight[4] = scalarField[ testMesh.cells2nodes[C,8] ];
+                       
+                    
+                       
+                    end
+                    
+                    # eps = 1.0e-12;
+                    # if phiLeft[1] < eps && phiRight[1] < eps
+
+                    #     phiLeft[1] =  phiRight[1] = scalarFieldCells[C];
+
+                    # elseif phiLeft[2] < eps && phiRight[2] < eps
+
+                    #     phiLeft[2] =  phiRight[2] = scalarFieldCells[C];
+
+                    # elseif phiLeft[3] < eps && phiRight[3] < eps
+
+                    #     phiLeft[3] =  phiRight[3] = scalarFieldCells[C];
+
+                    # elseif phiLeft[4] < eps && phiRight[4] < eps && testMesh.mesh_connectivity[C,3] == 4
+
+                    #     phiLeft[4] =  phiRight[4] = scalarFieldCells[C];
+                    # end
+
+
+                    
+                    phiFaceX[1] = 0.5*(phiLeft[1] + phiRight[1])*-ny[1]*side[1];		
+                    phiFaceY[1] = 0.5*(phiLeft[1] + phiRight[1])*-nx[1]*side[1];
+            
+                    phiFaceX[2] = 0.5*(phiLeft[2] + phiRight[2])*-ny[2]*side[2];		
+                    phiFaceY[2] = 0.5*(phiLeft[2] + phiRight[2])*-nx[2]*side[2];
+                    
+                    phiFaceX[3] = 0.5*(phiLeft[3] + phiRight[3])*-ny[3]*side[3];		
+                    phiFaceY[3] = 0.5*(phiLeft[3] + phiRight[3])*-nx[3]*side[3];
+                    
+                    phiFaceX[4] = 0.5*(phiLeft[4] + phiRight[4])*-ny[4]*side[4];		
+                    phiFaceY[4] = 0.5*(phiLeft[4] + phiRight[4])*-nx[4]*side[4];
+            
+                    gradX[C] =  (phiFaceX[1] + phiFaceX[2] + phiFaceX[3] + phiFaceX[4]) / testMesh.cell_areas[C];
+                    gradY[C] =  (phiFaceY[1] + phiFaceY[2] + phiFaceY[3] + phiFaceY[4]) / testMesh.cell_areas[C];
+                    
+        
+            
+                end ## end global loop for cells
+  
+            end ## threads
+
+            for B = 1:length(testMesh.bc_indexes)
+                BCell = testMesh.bc_data[B,1];
+                gradX[BCell] = gradY[BCell] = 0.0;    
+            end
+  
+  end
+
+
+
+
 function calcScalarFieldGradient(cellsThreads::Array{Int32,2}, testMesh::mesh2d_Int32, 
     scalarField::Array{Float64,1}, gradX::Array{Float64,1}, gradY::Array{Float64,1})
   
@@ -153,6 +288,8 @@ function testCalcGradients(meshname::String)
     # end
 
     calcScalarFieldGradient(cellsThreads,testMesh, UNodes, UGradXApprox, UGradYApprox);
+
+    calcScalarFieldGreenGaussCellBasedGradient(cellsThreads, testMesh, U, UNodes, UGradXApprox, UGradYApprox);
 
 
     for i = 1: NCells
